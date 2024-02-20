458ded0caeab5dd783118dd5254a1b0fdd7e1b46
Tando <23540850@sun.ac.za>
2024-02-20 22:38:48 +0200

    This is the proper project remove other backups

---
+++ EDesign314/Core/Inc/str_utils.h
@@ -0,0 +1,20 @@
+/*
+ * str_utils.h
+ *
+ *  Created on: Feb 20, 2024
+ *      Author: Tando
+ */
+#include <string.h>
+
+#ifndef INC_STR_UTILS_H_
+#define INC_STR_UTILS_H_
+
+//Variable declaration
+uint8_t student_number[] = "&_23540850_*\n";
+uint8_t data_from_computer[1];
+uint8_t time_elupsed[1];
+
+//Function declarations
+//uint8_t format_uart_response(uint8_t string);
+
+#endif /* INC_STR_UTILS_H_ */
